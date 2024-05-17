0126f46cadc98babe3b97052489d8f5fce10418f
Vostrikov Nikita <gamilforgpt@gmail.com>
2024-05-17 08:52:35 +0300

    Adding column to STUDENTS

--- SQL/table/lesson_2_task_2.sql
+++ SQL/table/lesson_2_task_2.sql
@@ -1,8 +1,6 @@
--- Check if the column "Exam Score" exists in the "Students" table
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS
                WHERE TABLE_NAME = 'Students' AND COLUMN_NAME = 'Exam Score')
 BEGIN
-    -- Add the column "Exam Score" to the "Students" table
     ALTER TABLE Students
     ADD [Exam Score] INT;
     
