4a4fe55b01d882b197a522c03f554ec7030c76f9
Vostrikov Nikita <gamilforgpt@gmail.com>
2024-06-14 12:56:26 +0300

    editin plugs

--- SQL/sp/plugPostgreSQL.sql
+++ SQL/sp/plugPostgreSQL.sql
@@ -1,4 +1,3 @@
--- Создаем заглушку функции в PostgreSQL
 CREATE OR REPLACE FUNCTION send_account_session(
     p_account_id INT,
     p_session_id INT
@@ -6,13 +5,10 @@ CREATE OR REPLACE FUNCTION send_account_session(
 DECLARE
     v_status INT;
 BEGIN
-    -- Здесь можно добавить любую логику, например, проверку значений или вставку в таблицу
-    -- В данном случае мы просто возвращаем фиктивный статус 0
     
     v_status = 0;
     RETURN v_status;
 END;
 $$ LANGUAGE plpgsql;
 
--- Пример вызова функции
 SELECT send_account_session(1, 123) AS Status;
